3c54103fa276aca16f5c2dcedfe4c5bf233e071c
Psalms91-50-23 <khothach@gmail.com>
2025-11-24 22:51:31 +1300

    change font size for smaller screens

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,14 +3,15 @@ import Link from "next/link";
 
 const NavBar =  () => (
   <nav className="flex w-full justify-between items-center p-4 bg-green-700 text-white">
-    <h1 className="font-bold text-xl">PrimCut Mowing</h1>
-    <ul className="flex flex-row space-x-4 list-none">
-      <li><Link href="/">Home</Link></li>
-      <li><Link href="/about">About</Link></li>
-      <li><Link href="/services">Services</Link></li>
-      <li><Link href="/pricing">Pricing</Link></li>
-      <li><Link href="/contact">Contact</Link></li>
+    <h1 className="font-bold text-xl title">PrimCut Mowing</h1>
+    <ul className="flex flex-row space-x-4 list-none text-base xs:text-sm font-size-change">
+      <li className="nav-link"><Link href="/">Home</Link></li>
+      <li className="nav-link"><Link href="/about">About</Link></li>
+      <li className="nav-link"><Link href="/services">Services</Link></li>
+      <li className="nav-link"><Link href="/pricing">Pricing</Link></li>
+      <li className="nav-link"><Link href="/contact">Contact</Link></li>
     </ul>
+
   </nav>
 );
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,18 @@
-content: [
-  "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
-]
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      xs: { max: "410px" }, // <= 410px
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+};
