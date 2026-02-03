93a292d08d72055c593891955b784eb637a779f2
Psalms91-50-23 <khothach@gmail.com>
2026-02-04 05:24:15 +1300

    removed unwanted unfinished pages code

--- components/CustomerDashBoard.tsx
+++ components/CustomerDashBoard.tsx
@@ -1,20 +1,21 @@
 
 import { Card, CardContent } from "@/components/ui/card";
+type CustomerDashboardProps = {
+  fullName: string;
+  user: {
+    id: string;
+    role: string;
+    email: string;
+    // add any other fields your `user` object has
+  };
+};
 
-
-export default function CustomerDashboard({ fullName, user }) {
+export default function CustomerDashboard({ fullName, user }: CustomerDashboardProps) {
   return (
     <div className="min-h-screen bg-gray-50 p-6 mt-20">
       <h1 className="text-3xl font-bold text-green-900">
         Welcome back {fullName}
       </h1>
-
-      {/* Profile */}
-      <ProfileCard user={user} />
-
-      {/* Customer Only */}
-      <CustomerQuotes />
-      <CustomerCompletedJobs />
     </div>
   );
 }
